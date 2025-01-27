75362c0ce970205a18a2fd71112bf445b9e2341c
patrisrikanth12 <patrisrikanth12@gmail.com>
2025-01-28 01:07:20 +0530

    setup router and server

--- cmd/students-api-golang/main.go
+++ cmd/students-api-golang/main.go
@@ -1,10 +1,35 @@
 package main
 
 import (
+	"fmt"
+	"log"
+	"net/http"
+
 	"github.com/patrisrikanth12/students-api-golang/internal/config"
 )
 
 func main() {
+	// 1. Load config
 	cfg := config.MustLoad()
-	
+
+	// 2. Load database
+
+	// 3. Setup Router
+	router := http.NewServeMux()
+
+	router.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Welcome to student API"))
+	})
+
+	// 4. Setup Server
+	server := http.Server{
+		Addr: cfg.HttpServer.Address,
+		Handler: router,
+	}
+
+	fmt.Printf("Server started: %s", cfg.HttpServer.Address)
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Fatal("Unable to start server")
+	}
 }
